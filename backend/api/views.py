d3ff038357d61c05a70f7b792b64504e3e619593
Andrey <shooroop87@mail.ru>
2024-05-24 15:56:08 +0300

    spanish + email + correctios v8

--- backend/api/views.py
+++ backend/api/views.py
@@ -8,7 +8,7 @@ import googlemaps
 from api.forms import DetailsForm, ExtrasForm, SearchForm, VehicleForm
 from api.models import Booking, Search
 from django.conf import settings
-from django.core.mail import EmailMultiAlternatives, send_mail
+from django.core.mail import EmailMultiAlternatives
 from django.shortcuts import redirect, render
 from django.template.loader import render_to_string
 from easy_pdf.rendering import render_to_pdf
@@ -582,14 +582,14 @@ def nexi(request):
     # Рендеринг HTML-шаблона
     html_content = render_to_string('booking/booking-received.html', context)
     # Отправка письма
-    #send_mail(
+    # send_mail(
     #    subject=subject,
     #    message='',  # Текстовое сообщение которые не поддерживают HTML
     #    from_email=settings.DEFAULT_FROM_EMAIL,
     #    recipient_list=to,
     #    html_message=html_content,
     #    fail_silently=False,
-    #)
+    # )
 
     # msg = EmailMultiAlternatives(subject, from_email, to)
     # msg.attach_alternative(html_content, "text/html")
