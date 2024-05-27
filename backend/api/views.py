48781b678f46e25160d5246c9ea5d9a35775f620
Andrey <shooroop87@mail.ru>
2024-05-28 00:15:13 +0300

    test pdf v4

--- backend/api/views.py
+++ backend/api/views.py
@@ -623,14 +623,12 @@ def link_callback(uri, rel):
         sRoot = settings.STATIC_ROOT
         mUrl = settings.MEDIA_URL
         mRoot = settings.MEDIA_ROOT
-        
         if uri.startswith(mUrl):
             path = os.path.join(mRoot, uri.replace(mUrl, ""))
         elif uri.startswith(sUrl):
             path = os.path.join(sRoot, uri.replace(sUrl, ""))
         else:
             return uri
-
         # make sure that file exists
         if not os.path.isfile(path):
             raise RuntimeError(
